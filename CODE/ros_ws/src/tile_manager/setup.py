511d64cb8e4db729a3ed06836502d902219cac8c
tejasmk-tkp <dawn3tkp@gmail.com>
2026-02-12 21:58:02 +0530

    update benchmark results

--- CODE/ros_ws/src/tile_manager/setup.py
+++ CODE/ros_ws/src/tile_manager/setup.py
@@ -2,39 +2,37 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'tile_manager'
+package_name = "tile_manager"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'maps'),
-            glob('maps/*.yaml') + glob('maps/*.pgm')),
-        (os.path.join('share', package_name, 'config'),
-            glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "maps"),
+            glob("maps/*.yaml") + glob("maps/*.pgm"),
+        ),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='myrobo',
-    maintainer_email='myrobo@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="myrobo",
+    maintainer_email="myrobo@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-                'tile_switcher = tile_manager.tile_switcher:main',
-                'benchmark = tile_manager.benchmark_node:main',
-                'benchmark_visualizer = tile_manager.benchmark_visualizer_node:main',
+        "console_scripts": [
+            "tile_switcher = tile_manager.tile_switcher:main",
+            "benchmark = tile_manager.benchmark_node:main",
         ],
     },
 )
