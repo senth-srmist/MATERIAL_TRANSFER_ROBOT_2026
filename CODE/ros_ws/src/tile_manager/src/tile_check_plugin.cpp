1ae87a0f8286ab815461c0810bee14a949c4bc07
tejasmk-tkp <dawn3tkp@gmail.com>
2026-02-15 14:00:15 +0530

    tile switch bt

--- CODE/ros_ws/src/tile_manager/src/tile_check_plugin.cpp
+++ CODE/ros_ws/src/tile_manager/src/tile_check_plugin.cpp
@@ -1,13 +1,20 @@
 /**
  * @file tile_check_plugin.cpp
- * @brief Register TileCheckAction as a BT plugin
+ * @brief Register TileCheckAction as a Nav2 BT plugin
+ * 
+ * Note: We explicitly define BT_RegisterNodesFromPlugin with proper
+ * visibility attributes instead of using BT_REGISTER_NODES macro,
+ * which may define it as static in some configurations.
  */
 
 #include "tile_manager/tile_check_action.hpp"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
-// Register the node with BehaviorTree.CPP
-BT_REGISTER_NODES(factory)
-{
-  factory.registerNodeType<tile_manager::TileCheckAction>("TileCheckAction");
+// Explicitly export the registration function with C linkage and default visibility
+extern "C" {
+  void __attribute__((visibility("default"))) 
+  BT_RegisterNodesFromPlugin(BT::BehaviorTreeFactory& factory)
+  {
+    factory.registerNodeType<tile_manager::TileCheckAction>("TileCheckAction");
+  }
 }
