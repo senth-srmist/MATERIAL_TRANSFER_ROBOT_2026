5cabce6373522a88db3a2e8981e5327a1aba6a79
tejasmk-tkp <dawn3tkp@gmail.com>
2026-02-17 20:21:48 +0530

    change baud rate to 2400

--- CODE/ros_ws/src/sabertooth_diff_drive/sabertooth_diff_drive/sabertooth_diff_drive_node.py
+++ CODE/ros_ws/src/sabertooth_diff_drive/sabertooth_diff_drive/sabertooth_diff_drive_node.py
@@ -7,19 +7,20 @@ import serial
 import time
 
 PORT = "/dev/ttyUSB0"
-BAUD = 9600
+BAUD = 2400
 
 
 class SabertoothDiffDrive(Node):
+
     def __init__(self):
         super().__init__("sabertooth_diff_drive")
 
         # Use QoS with depth=1 to always get latest command
         qos = QoSProfile(depth=1, reliability=ReliabilityPolicy.BEST_EFFORT)
 
-        self.subscription = self.create_subscription(
-            Twist, "/cmd_vel", self.cmd_vel_callback, qos
-        )
+        self.subscription = self.create_subscription(Twist, "/cmd_vel",
+                                                     self.cmd_vel_callback,
+                                                     qos)
 
         self.is_stopped = True
 
