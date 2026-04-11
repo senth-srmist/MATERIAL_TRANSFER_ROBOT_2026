8b111b3fae4cb42341320bb6a50d3e6d30acb419
tejasmk-tkp <dawn3tkp@gmail.com>
2026-04-11 14:11:46 +0530

    chore: delete old zed config and update install

--- CODE/ros_ws/src/robot_bringup/setup.py
+++ CODE/ros_ws/src/robot_bringup/setup.py
@@ -12,7 +12,10 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
-        (os.path.join("share", package_name, "config"), glob("config/zedm.yaml")),
+        (
+            os.path.join("share", package_name, "config"),
+            glob("config/zedm.yaml"),
+        ),
         (
             os.path.join("share", package_name, "urdf"),
             glob("urdf/*.xacro") + glob("urdf/*.urdf"),
